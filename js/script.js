b2c8600cad02052ed8a1dfdb744b4d605ccd1371
MicheleSerafini <serafini.michele01@gmail.com>
2023-10-25 13:25:10 +0200

    ADD > Starting setUp [index/style/script]

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const { createApp } = Vue;
+
+createApp({
+
+  data() {
+    return {
+      //variabili
+    }
+  },
+
+  methods: {
+    //funzioni
+  },
+
+  mounted() {
+    console.log('>> vue working <<');
+  }
+
+}).mount('#app');
