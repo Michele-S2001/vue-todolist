d264e4ac3faa98f39dff22cab1b7daa046b1468f
MicheleSerafini <serafini.michele01@gmail.com>
2023-10-25 15:28:32 +0200

    FEAT > Milestone 1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # TODO LIST WITH OBJECTS
+
+- creare lo stile statico della pagina
+
+- fare in modo di generare le task in base agli oggetti presenti nell'array todos
+
+- creare la condizione per il quale se non ci sono task mi appare il messaggio che me lo comunica
+
+- creare una funzione che cambi il valore di done degli oggetti in modo tale da applicare una classe che sbarri la task
--- js/script.js
+++ js/script.js
@@ -4,17 +4,38 @@ createApp({
 
   data() {
     return {
-      todos: [
-        {
-          text: 'fare i compiti',
-          done: false
-        }
-      ]
+      todos: [],
+      taskInput: '',
     }
   },
 
   methods: {
-    //funzioni
+    // genero le task se ce n'è almeno una nell'array
+    isThereTasks() {
+      let thereAreTasks = false;
+      if(this.todos.length > 0) {
+        thereAreTasks = true;
+      } 
+      return thereAreTasks;
+    },
+    // aggiungo la nuova task all'array
+    addTask() {
+      this.todos.unshift(
+        {
+          text: this.taskInput,
+          done: false
+        }
+      );
+      this.taskInput = '';
+    },
+    // sbarro la task se completata
+    isDone(indexTask) {
+      if(this.todos[indexTask].done === false) {
+        this.todos[indexTask].done = true
+      } else {
+        this.todos[indexTask].done = false
+      }
+    },
   },
 
   mounted() {
